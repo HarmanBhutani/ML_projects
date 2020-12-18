21af90a002aedc8818f5195211d94d9db3a77a26
Harman Bhutani <byteharman0101@gmail.com>
2020-12-18 14:50:42 -0500

    Create ConfidenceInterval.py

---
+++ ConfidenceInterval.py
@@ -0,0 +1,6 @@
+def mean_confidence_interval(data, confidence=0.95):
+    a = 1.0 * np.array(data)
+    n = len(a)
+    m, se = np.mean(a), scipy.stats.sem(a)
+    h = se * scipy.stats.t.ppf((1 + confidence) / 2., n-1)
+    return m, m-h, m+h
